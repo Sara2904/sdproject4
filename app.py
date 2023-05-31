d8067390a261d8f08a307b1bb2415dd435494817
Sara2904 <sararojas29@icloud.com>
2023-05-31 19:03:33 -0400

    app

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
-
 
 import pandas as pd
 import numpy as np
@@ -11,65 +9,19 @@ import plotly.express as px
 import streamlit as st
 
 
-# In[16]:
-
-
 df = pd.read_csv('/Users/sarar/sdproject4/vehicles_us.csv')
-
-
-# In[3]:
-
-
-df.head()
-
-
-# In[4]:
-
-
-df.info()
-
-
-# In[5]:
-
-
-df.describe()
-
-
-# In[6]:
-
-
-df.isnull().sum()
-
-
-# In[7]:
-
 
 figl = px.histogram(df, x='price', nbins=50)
 figl.show()
 
-
-# In[8]:
-
-
 fig2 = px.histogram(df, x='odometer', nbins=30)
 fig2.show()
-
-
-# In[9]:
 
 fig3 = px.scatter(df, x='odometer', y='price')
 fig3.show()
 
-
-# In[14]:
-
-
 fig4 = px.scatter(df, x='model_year', y='price')
 fig4.show()
-
-
-# In[13]:
-
 
 st.header("Car Advertisement Dataset Analysis")
 
